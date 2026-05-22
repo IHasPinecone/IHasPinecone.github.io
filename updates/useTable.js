fdf158919295008f81b0fc60b1e4352d9adfc057
IHasPinecone <68713926+IHasPinecone@users.noreply.github.com>
2026-05-22 16:22:30 -0400

    Update and rename useTable.js to generatePhrases

--- updates/useTable.js
+++
@@ -1,8 +0,0 @@
-export function useTable(table1, seed) {
-  const seedtest = Math.floor(Date.now() / 864000000);
-  const hash = simpleHash(seed.toString());
-
-  return [
-    pickWord(hash.slice(0,3), table)
-    ];
-}
