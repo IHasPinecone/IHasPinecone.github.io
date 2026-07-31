60251fb8ece4a29318271be7e655f8803bfb7dba
IHasPinecone <68713926+IHasPinecone@users.noreply.github.com>
2026-07-31 11:18:08 -0400

    Update updates.js

--- updates/updates.js
+++ updates/updates.js
@@ -170,7 +170,7 @@ function formatSeed(date) {
   function linkify(text) {
   return text.replace(
     /(https?:\/\/[^\s]+)/g,
-    url => `<a href{url}>link</a>`
+    url => `<a href="${url}>`
   );
 }
 
