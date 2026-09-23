5c056678da0a25506a6da68a41e601197b52dadd
IHasPinecone <68713926+IHasPinecone@users.noreply.github.com>
2026-09-23 17:39:58 -0400

    Fix URL formatting for episode image in RSS feed

--- generate-rss.js
+++ generate-rss.js
@@ -50,7 +50,7 @@ function buildPodcastItems(feedPosts) {
     const postUrl = `${SITE_URL}/${post.link}`;
 
     const episodeImage = post.coverImage
-      ? `${<SITE_URL}/${post.coverImage}`
+      ? `${SITE_URL}/${post.coverImage}`
       : "";
 
     return `
